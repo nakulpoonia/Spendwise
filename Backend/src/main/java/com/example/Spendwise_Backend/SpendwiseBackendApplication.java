2b3f97ebb4568dd2d78d10ffef1800e045934c20
Nakulpoonia <poonianakul@gmail.com>
2026-09-11 15:17:01 +0530

    intial commit

---
+++ Backend/src/main/java/com/example/Spendwise_Backend/SpendwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spendwise_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendwiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendwiseBackendApplication.class, args);
+	}
+
+}
